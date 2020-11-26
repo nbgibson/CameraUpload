b33c61de49e458fa3d34c0a58c6d8876387cb9f4
Nathan <burdwerks@gmail.com>
2020-11-26 12:31:26 -0500

    init

---
+++ cameraupload.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cameradir=/Volumes/NO\ NAME/DCIM/
+for D in `find "$cameradir" -type d`
+do
+  if [[ "$D" == "$cameradir" ]]; then
+    continue
+  else
+    echo "D: " "$D"
+    date=$(stat -f "%Sm" -t "%y.%m.%d" "$D")
+    echo "date: " "$date"
+    scp -r "$D" homelan:/media/NAS/nathan/Photos/GoodCamera/"$date"
+  fi
+done
+
+ssh homelan /home/nate/bin/moveraws
