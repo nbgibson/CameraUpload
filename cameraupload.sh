bc72d052947b16484d9f8eca1791430210136400
Nathan <burdwerks@gmail.com>
2020-11-26 13:24:02 -0500

    ln 4 hates spaces, tweaked methodlogy

--- cameraupload.sh
+++ cameraupload.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-cameradir="/Volumes/NO\ NAME/DCIM/"
-cd $cameradir
+cameradir='/Volumes/NO NAME/DCIM/'
+cd "$cameradir"
 for dir in */
 do
-  if [[ "$dir" == "$cameradir" ]]; then
-    continue
-  else
-    echo "dir: " "$dir"
-    date=$(stat -f "%Sm" -t "%y.%m.%d" "$dir")
-    echo "date: " "$date"
-    scp -r "$dir" homelan:/media/NAS/nathan/Photos/GoodCamera/"$date"
-  fi
+  echo "dir: " "$dir"
+  date=$(stat -f "%Sm" -t "%y.%m.%d" "$dir")
+  echo "date: " "$date"
+  scp -r "$dir" homelan:/media/NAS/nathan/Photos/GoodCamera/"$date"
 done
 #Trigger rsync script to move raws to NAS
 ssh homelan /home/nate/bin/moveraws
