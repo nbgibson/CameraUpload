d6cdf3f44b711074eab467b0d18acc74c3083551
Nathan <burdwerks@gmail.com>
2021-05-21 23:09:20 -0400

    NAS rework

--- cameraupload.sh
+++ cameraupload.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 cameradir='/Volumes/NO NAME/DCIM/'
 cd "$cameradir" || exit
-echo "========================= Moving photos to homelan ========================"
+echo "========================= Moving photos to NAS pool ========================"
 for dir in */
 do
   echo "dir: " "$dir"
   date=$(stat -f "%Sm" -t "%y.%m.%d" "$dir")
   echo "date: " "$date"
-  scp -r "$dir" homelan:/media/NAS/nathan/Photos/GoodCamera/"$date"
+  scp -r "$dir" homenas:/volume1/NAS/nathan/Photos/GoodCamera/"$date"
 done
 #Trigger rsync script to move raws to NAS
-echo "========================= Moving photos to NAS ========================"
-ssh homelan /home/nate/bin/moveraws
+echo "========================= Sorting and moving Raws ========================"
+ssh homenas /var/services/homes/nate/moveraws.sh
 #Trigger reindex of Photos dir(s) to have them appear in PhotoStation
 echo "========================= Reindex Photos on NAS ========================"
 ssh homenas 'find /var/services/photo/DSLR/ -mindepth 1 -maxdepth 1 -type d -mtime -1 -exec /usr/syno/bin/synoindex -R photo -A {} \;'
---
+++ moveraws.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo "##########Move to Raws folder##########"
+rsync -ahzi --ignore-existing --progress --exclude '*.JPG' --exclude '*.MP4' --prune-empty-dirs /volume1/NAS/nathan/Photos/GoodCamera /volume1/NAS/nathan/Photos/Raws
+echo "##########Purge defunct Raws##########"
+find /volume1/NAS/nathan/Photos/GoodCamera -name "*.ARW" -type f -delete
+echo "##########Port Raws to Photostation##########"
+rsync -ahzi --ignore-existing --progress --prune-empty-dirs /volume1/NAS/nathan/Photos/Raws/ homenas:/volume1/photo/DSLR/
