19a5730a16bbb250dd992db16f87edf5a8180e72
Nathan <burdwerks@gmail.com>
2021-09-05 12:20:09 -0400

    Pulled down current moveraws script

--- moveraws.sh
+++ moveraws.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "##########Move to Raws folder##########"
-rsync -ahzi --ignore-existing --progress --exclude '*.JPG' --exclude '*.MP4' --prune-empty-dirs /volume1/NAS/nathan/Photos/GoodCamera/ /volume1/NAS/nathan/Photos/Raws
+rsync -ahzi --ignore-existing --progress --exclude '*.JPG' --exclude '*.MP4' --prune-empty-dirs /var/services/homes/nate/Photos/GoodCamera/ /var/services/homes/nate/Photos/Raws
 echo "##########Purge defunct Raws##########"
-find /volume1/NAS/nathan/Photos/GoodCamera -name "*.ARW" -type f -delete
-echo "##########Port Raws to Photostation##########"
-rsync -ahzi --ignore-existing --progress --prune-empty-dirs /volume1/NAS/nathan/Photos/Raws/ /volume1/photo/DSLR/
+find /var/services/homes/nate/Photos/GoodCamera -name "*.ARW" -type f -delete
+#echo "##########Port Raws to Photostation##########"
+#rsync -ahzi --ignore-existing --progress --prune-empty-dirs /var/services/homes/nate/Photos/Raws/ /volume1/photo/DSLR/
