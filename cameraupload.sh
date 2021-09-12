09cf46cc6bae15b84632032a1ec58bed4385706c
Nathan <burdwerks@gmail.com>
2021-09-11 21:50:08 -0400

    Lack of JPGs and new structure rework

--- cameraupload.sh
+++ cameraupload.sh
@@ -7,11 +7,11 @@ do
   echo "dir: " "$dir"
   date=$(stat -f "%Sm" -t "%y.%m.%d" "$dir")
   echo "date: " "$date"
-  scp -r "$dir" homenas:/var/services/homes/nate/Photos/GoodCamera/"$date"
+  scp -r "$dir" homenas:/var/services/homes/nate/Photos/Raws/"$date"
 done
 #Trigger rsync script to move raws to NAS
-echo "========================= Sorting and moving Raws ========================"
-ssh homenas /var/services/homes/nate/moveraws.sh
+#echo "========================= Sorting and moving Raws ========================"
+#ssh homenas /var/services/homes/nate/moveraws.sh
 #Trigger reindex of Photos dir(s) to have them appear in PhotoStation
 echo "========================= Reindex Photos on NAS ========================"
 ssh homenas 'find /var/services/homes/nate/Photos/Raws/ -mindepth 1 -maxdepth 1 -type d -mtime -1 -exec /usr/syno/bin/synoindex -R photo -A {} \;'
--- moveraws.sh
+++ moveraws.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-echo "##########Move to Raws folder##########"
-rsync -ahzi --ignore-existing --progress --exclude '*.JPG' --exclude '*.MP4' --prune-empty-dirs /var/services/homes/nate/Photos/GoodCamera/ /var/services/homes/nate/Photos/Raws
-echo "##########Purge defunct Raws##########"
-find /var/services/homes/nate/Photos/GoodCamera -name "*.ARW" -type f -delete
+#echo "##########Move to Raws folder##########"
+#rsync -ahzi --ignore-existing --progress --exclude '*.JPG' --exclude '*.MP4' --prune-empty-dirs /var/services/homes/nate/Photos/GoodCamera/ /var/services/homes/nate/Photos/Raws
+#echo "##########Purge defunct Raws##########"
+#find /var/services/homes/nate/Photos/GoodCamera -name "*.ARW" -type f -delete
 #echo "##########Port Raws to Photostation##########"
 #rsync -ahzi --ignore-existing --progress --prune-empty-dirs /var/services/homes/nate/Photos/Raws/ /volume1/photo/DSLR/
